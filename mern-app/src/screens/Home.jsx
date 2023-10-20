5f0f58f18d59799dbb045e0ee31698ee2ce71bb4
varsh06pm <varsha.pm123@gmail.com>
2023-10-20 16:54:15 +0530

    food

---
+++ mern-app/src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+
+
+export default function Home() {
+  return (
+    <div>
+        <div><NavBar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+    </div>
+  )
+}
